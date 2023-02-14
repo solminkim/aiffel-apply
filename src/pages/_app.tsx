3cbe109095613a9472ae613060d7e57de0f37606
solminkim <rlathfals11@gmail.com>
2023-02-14 19:04:50 +0900

    second commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,6 @@
 import '../styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import type { NextWebVitalsMetric } from 'next/app'
-export function reportWebVitals(metric: NextWebVitalsMetric) {
-  console.log(metric)
-}
 
 import Layout from '../components/Layout';
 import type { AppProps } from 'next/app';
